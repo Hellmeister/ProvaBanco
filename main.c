3346936a0707f3f772ea9ae7d19c6ad4b0e1aa1a
Hellmeister <jv.hellmeister@hotmail.com>
2016-11-10 19:05:36 -0200

    Thrid Alteration

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ struct cadastro
 
 int main()
 {
-    printf("Rapalo, quantas contas deseja cadastrar?\n");
+    printf("Rapalo Viadinho, quantas contas deseja cadastrar?\n");
     scanf("%d", &ncontas);
     do
     {
